a1705713ab07af379af0f2dfbde01de1044316d3
Santiago R <82001635+prrox@users.noreply.github.com>
2025-01-25 12:24:21 -0500

    Delete proyecto_Grupo4 directory

--- proyecto_Grupo4/src/shared/lib/env.ts
+++
@@ -1,9 +0,0 @@
-export const Appwrite = {
-    // projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
-
-
-    // databaseId: import.meta.env.VITE_APPWRITE_STORE_DATABASE_ID,
-    // collections: {
-    //     products: import.meta.env.VITE_APPWRITE_PRODUCTS_COLLECTION_ID
-    // }
-}
